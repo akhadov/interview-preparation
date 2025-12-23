d863134584da07666c38169d214338a974a9a919
abdullah-collab-work <abdullah.corporatework@gmail.com>
2025-12-23 15:54:48 +0500

    refactor: remove MyController, MyService, MyRepository, and MyModel classes

--- task/BlogPost/BlogPost.Api/DTOs/BlogDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlogPost.Api.DTOs;
-
-public record BlogDto
-{
-    public int BlogId { get; set; }
-    public string Name { get; set; }
-    public bool IsActive { get; set; }
-    public List<PostDto> Articles { get; set; } = new();
-}
